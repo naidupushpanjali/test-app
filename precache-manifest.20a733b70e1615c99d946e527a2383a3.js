119e4c44e5ae5578e49265ea9e0208e241ec25dd
= <=>
2020-05-11 23:24:50 +0530

    Updates

---
+++ precache-manifest.20a733b70e1615c99d946e527a2383a3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a0904e4b6397bfd83bfa034e1e34179",
+    "url": "/test-app/index.html"
+  },
+  {
+    "revision": "f649f3eeb6872a2f0028",
+    "url": "/test-app/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "c85670493d155f46c8b0",
+    "url": "/test-app/static/css/main.f2a438cb.chunk.css"
+  },
+  {
+    "revision": "f649f3eeb6872a2f0028",
+    "url": "/test-app/static/js/2.cee81ff5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/test-app/static/js/2.cee81ff5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c85670493d155f46c8b0",
+    "url": "/test-app/static/js/main.fe98eec1.chunk.js"
+  },
+  {
+    "revision": "42f8bc4754f4b0c9cce4",
+    "url": "/test-app/static/js/runtime-main.847fb34c.js"
+  },
+  {
+    "revision": "782f7b1e4ae57dcd985c935f36fab7f4",
+    "url": "/test-app/static/media/Poppins-Regular.782f7b1e.woff"
+  },
+  {
+    "revision": "22cac2caa872455aa6c9831b68da2ea6",
+    "url": "/test-app/static/media/Poppins-SemiBold.22cac2ca.woff"
+  }
+]);
